2ed261b7e28b54096bffe9945b16c83ed55f62db
cankur760 <cankur760@gmail.com>
2018-06-22 08:49:41 +0000

    first commit

--- myloginproject/myapplication/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyapplicationConfig(AppConfig):
-    name = 'myapplication'
--- myloginproject/settings.py
+++ myloginproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myloginproject',
-    'templates',
+    'myapplication',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST':'http://127.0.0.1:8000',
     }
 }
 
